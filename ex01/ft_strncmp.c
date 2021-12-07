8ee12938721b7f2968195d1225e6a1be856ffe50
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-07 22:51:38 +1030

    update

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -5,18 +5,21 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
     int i;
     
     i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0')
+    while (i <= n)
     {
-        while (s1[i] == s2[i])
+        while (s1[i] != '\0' && s2[i] != '\0')
         {
-            if (s1[i] <= n && s2[i] <= n)
+            if (s1[i] == s2[i])
             {                
-               return (1);
-            }                                                                 
-            i++;
+               return (0);
+            } 
+            {
+                i++;
+            }                                                                
         }
+           
     }
-    return (0);
+    return (1);
 }
 
 int main()
