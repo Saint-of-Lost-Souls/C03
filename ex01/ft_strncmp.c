a57411370727df372258b0a26f73bc0b47902100
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-08 16:37:23 +1030

    Upload

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -5,34 +5,31 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
     int i;
     
     i = 0;
-    while (i <= n)
-    {
-        while (s1[i] != '\0' && s2[i] != '\0')
-        {
-            if (s1[i] == s2[i])
-            {                
-               return (0);
-            } 
-            {
-                i++;
-            }                                                                
+    while (s1[i] != '\0' && s2[i] != '\0' && i <= n)
+    {        
+        if (s1[i] != s2[i])
+        {       
+            return (1);
         }
-           
-    }
-    return (1);
+        else
+        {
+            i++;                                                                
+        }                
+    }    
+    return (0);
 }
 
 int main()
 {
     char string_1[] = "the cake is a lie";
-    char string_2[] = "the cake is pizza";
-    unsigned int n = 15;
+    char string_2[] = "the pizza is cake";
+    unsigned int n = 3;
 
     int result = ft_strncmp(string_1, string_2, n);
 
     if (result == 0)
     {
-        printf("There strings are the same");
+        printf("These strings are the same, but competely different");
     }
     else
     {
---
+++ ex02/ft_strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char    *ft_strcat(char *dest, char *src)
+{
+
+
+}
+
+
+int main()
+{
+    char dest[] = "The cake is a lie";
+    char src[] = "And so are you";
+
+    ft_strcat(dest, src);
+
+    return 0;
+}
+
