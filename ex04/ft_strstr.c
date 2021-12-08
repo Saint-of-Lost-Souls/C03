26cf9a3945734f43132484083a8a3521a5b4a056
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-08 20:37:01 +1030

    update

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 
 char    *ft_strstr(char *str, char *tofind)
-{
-    // append dest to put src at end
+{    
     int x;
     int y;
-    x = 0;
-    y = 0;
+    int c;
 
-    while(dest[x] != '\0')
+    while (str[x] != '\0')
     {
-        x++;
+        if (str[x] != tofind[y])
+        {
+            x++;
+        }
+        else if(str[x] == tofind[y])
+        {
+            while (tofind[y] != '\0')
+            {
+                if (str[x] == tofind[y])
+                {
+                    x++;
+                    y++;
+                    c++;
+                }
+                else
+                {                        
+                    break;
+                }
+            }
+        }
+        
     }
-    while(src[y] != '\0')
-    {
-        dest[x] = src[y];
-        x++;
-        y++;
-    }
-    dest[x] = '\0';     
-    return (dest);
+    return (str);
 }
 
 int main()
 {
-    char str[] = "The cake is a lie";
-    char str_1[] = "And so are you";
+    char str[] = "chocolatecake, carrotcake, pancake, donuts, muffin";
+    char tofind[] = "cake";
     
     ft_strstr(str, tofind);
+
 
     printf("The complete string is: %s", str);
     return 0;
