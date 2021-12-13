b8bbca4ca6bd3ee3575cf535d337bac43bb88ec1
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-13 13:48:56 +1030

    Update ft_strncat.c

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -5,11 +5,11 @@ char    *ft_strncat(char *dest, char *src, unsigned int n)
     int i;
     int j;
         
-    while(dest[i] != '\0')
+    while (dest[i] != '\0')
     {
         i++;
     }
-    while(src[j] != '\0' && j < n)       
+    while (src[j] != '\0' && j < n)       
     {
         dest[i + j] = src[j];        
         j++;               
