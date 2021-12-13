1c362665951646c60414f080650e0f52ec461f08
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-13 14:08:13 +1030

    ok

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -5,7 +5,8 @@ char    *ft_strncat(char *dest, char *src, unsigned int n)
     unsigned int i;
     unsigned int j;
 
-           
+    i = 0;
+    j = 0;           
     while (dest[i] != '\0')
     {
         i++;
