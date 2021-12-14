04e5df6494fdc1961386d2950e00c80b46bdd89e
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 21:49:14 +1030

    Update ft_strcmp.c

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int ft_strcmp(char *s1, char *s2)
-{    
-	int	    i;
-
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i])
-		i++;
-	return (s1[i] - s2[i]);
-} 
-     
-
+{
+    int i;
+       
+    i = 0;
+    while (s1[i] && s2[i])
+    {
+        if (s1[i] != s2[i])
+        {
+            return (1);                
+        }
+        i++;
+    }
+    return (0);   
+}  
+    
 int main()  
 {  
    char str1[] = "the cake is a lie"; // declaration of char array  
