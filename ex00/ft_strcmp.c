a0d78509b0c0913309b79d9c550a398682679d0f
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-15 18:24:21 +1030

    ok

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 
-int ft_strcmp(char *s1, char *s2)
+int     ft_strcmp(char *s1, char *s2)
 {
-    int i;
-       
-    i = 0;
-    while (s1[i] && s2[i])
-    {
-        if (s1[i] > s2[i])
+        int     i;
+
+        i = 0;
+        while (s1[i] == s2[i] && s1[i] && s2[i])
         {
-            return (-1);                
+                i++;
         }
-        else if (s1[i] < s2[i])
-        {
-            return (1);
-        }
-        i++;
-    }
-    return (0);   
-}  
+        return (s1[i] - s2[i]);
+}
     
 int main()  
 {  
